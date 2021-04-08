abf251d232cef8aebc36600c6aaa62eec04c7507
sohil672007 <71943811+sohil672007@users.noreply.github.com>
2021-04-08 19:45:27 +0530

    Add files via upload

---
+++ GuessingNumbers.py
@@ -0,0 +1,12 @@
+intro = int(input("Guess a random Number between 10 to 15"))
+
+if (intro>14):
+    print("You Win ")
+
+elif(intro>12):
+    print("Almost There")
+else:
+    print("NO")
+
+     
+
